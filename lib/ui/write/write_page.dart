7c2b23b43c55202895f946021d19e5ea89c7c4e7
hippieperm <leess0133@gmail.com>
2025-04-15 01:22:59 +0900

    Flutter Firebase 블로그 앱의 세부 페이지, 홈 페이지 및 작성 페이지 구현

---
+++ lib/ui/write/write_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class WritePage extends StatelessWidget {
+  const WritePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Text('WritePage'),
+    );
+  }
+}
