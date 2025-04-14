9dba43f7eb5abc974fe45dd5a5c27875a2d4ed04
hippieperm <leess0133@gmail.com>
2025-04-15 02:28:00 +0900

    ✨ 홈 페이지의 리스트 뷰를 개선하고, 각 아이템 클릭 시 세부 페이지로 이동하는 기능 추가

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_blog_app/ui/detail/detail_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -18,71 +19,91 @@ class HomePage extends StatelessWidget {
             const Text('최근글', style: TextStyle(fontWeight: FontWeight.bold)),
             const SizedBox(height: 20),
             Expanded(
-              child: ListView(
-                children: [
-                  SizedBox(
-                    width: double.infinity,
-                    height: 120,
-                    child: Stack(
-                      children: [
-                        Positioned(
-                          width: 120,
-                          height: 120,
-                          right: 0,
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(20),
-                            child: Image.network(
-                              'https://picsum.photos/200/300',
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                        ),
-                        Container(
-                          width: double.infinity,
-                          height: double.infinity,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          margin: const EdgeInsets.only(right: 100),
-                          padding: const EdgeInsets.all(20),
-                          child: const Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'data',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 14,
-                                ),
-                              ),
-                              Spacer(),
-                              Text(
-                                'data',
-                                style: TextStyle(
-                                  color: Colors.grey,
-                                  fontSize: 12,
-                                ),
-                              ),
-                              Text(
-                                'data',
-                                style: TextStyle(
-                                  color: Colors.grey,
-                                  fontSize: 12,
-                                ),
-                              ),
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                ],
+              child: ListView.separated(
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return item();
+                },
+                separatorBuilder: (context, index) =>
+                    const SizedBox(height: 10),
               ),
             ),
           ],
         ),
       ),
     );
+  }
+
+  Widget item() {
+    return Builder(builder: (context) {
+      return GestureDetector(
+        onTap: () {
+          Navigator.push(context, MaterialPageRoute(
+            builder: (context) {
+              return const DetailPage();
+            },
+          ));
+        },
+        child: SizedBox(
+          width: double.infinity,
+          height: 120,
+          child: Stack(
+            children: [
+              Positioned(
+                width: 120,
+                height: 120,
+                right: 0,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.network(
+                    'https://picsum.photos/200/300',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                height: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                margin: const EdgeInsets.only(right: 100),
+                padding: const EdgeInsets.all(20),
+                child: const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Today I Learend',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14,
+                      ),
+                    ),
+                    Spacer(),
+                    Text(
+                      '플러터 그리드 뷰를 배웠습니다.플러터 그리드 뷰를 배웠습니다.플러터 그리드 뷰를 배웠습니다.',
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 12,
+                      ),
+                    ),
+                    SizedBox(height: 4),
+                    Text(
+                      '2024.08.18. 20:34',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 12,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      );
+    });
   }
 }
