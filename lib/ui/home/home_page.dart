fbc6bbaebea06468f57b54c4d00bc4cab93c8df5
hippieperm <leess0133@gmail.com>
2025-04-15 01:48:04 +0900

    홈 페이지 UI 개선: 배경색 변경, 앱바 제목 추가 및 최근글 섹션 구현

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -6,8 +6,55 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: const Text('HomePage'),
+      backgroundColor: Colors.grey[300],
+      appBar: AppBar(
+        title: const Text('BLOG'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text('최근글', style: TextStyle(fontWeight: FontWeight.bold)),
+            const SizedBox(height: 20),
+            Expanded(
+              child: ListView(
+                children: [
+                  SizedBox(
+                    width: double.infinity,
+                    height: 120,
+                    child: Stack(
+                      children: [
+                        Positioned(
+                          width: 120,
+                          height: 120,
+                          right: 0,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Image.network(
+                              'https://picsum.photos/200/300',
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          width: double.infinity,
+                          height: double.infinity,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          margin: const EdgeInsets.only(right: 100),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
