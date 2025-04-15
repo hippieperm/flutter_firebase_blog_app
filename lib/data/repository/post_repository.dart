cda80df0ab49f466ce1cb1f73c8cfd6b334d64bb
hippieperm <leess0133@gmail.com>
2025-04-16 02:35:15 +0900

    ✨ PostRepository 클래스의 모든 포스트 가져오기 기능에 예외 처리 추가: Firestore에서 포스트 데이터를 가져오는 과정에서 발생할 수 있는 오류를 처리하고, 반환 타입을 수정하여 null을 허용하도록 변경

--- lib/data/repository/post_repository.dart
+++ lib/data/repository/post_repository.dart
@@ -2,24 +2,29 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_firebase_blog_app/data/model/post.dart';
 
 class PostRepository {
-  Future<List<Post>> getAll() async {
-    final firestore = FirebaseFirestore.instance;
-    final collectionRef = firestore.collection('posts');
-    final result = await collectionRef.get();
+  Future<List<Post>?> getAll() async {
+    try {
+      final firestore = FirebaseFirestore.instance;
+      final collectionRef = firestore.collection('posts');
+      final result = await collectionRef.get();
 
-    final docs = result.docs;
+      final docs = result.docs;
 
-    return docs.map((doc) {
-      final map = doc.data();
-      final newMap = {
-        'id': doc.id,
-        ...map,
-      };
-      return Post.fromJson(newMap);
-    }).toList();
+      return docs.map((doc) {
+        final map = doc.data();
+        final newMap = {
+          'id': doc.id,
+          ...map,
+        };
+        return Post.fromJson(newMap);
+      }).toList();
+    } catch (e) {
+      print(e);
+      return null;
+    }
   }
 
-  Future<void> insert({
+  Future<bool> insert({
     required String title,
     required String content,
     required String writer,
@@ -36,9 +41,10 @@ class PostRepository {
         'writer': writer,
         'imageUrl': imageUrl,
       });
+      return true;
     } catch (e) {
       print(e);
-      throw Exception(e);
+      return false;
     }
   }
 }
