cf6fda080d59476f613609b08a4c5ac65fb3b26e
hippieperm <leess0133@gmail.com>
2025-04-15 11:22:44 +0900

    ✨ 세부 페이지에 이미지 및 리스트 뷰 추가: 아이콘 버튼과 텍스트 정보 구성

--- lib/ui/detail/detail_page.dart
+++ lib/ui/detail/detail_page.dart
@@ -6,8 +6,53 @@ class DetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: const Text('DetailPage'),
+      appBar: AppBar(
+        actions: [
+          iconButton(
+            Icons.delete,
+            () {},
+          ),
+          iconButton(
+            Icons.edit,
+            () {},
+          ),
+        ],
+      ),
+      body: ListView(
+        children: [
+          Image.network(
+            'https://picsum.photos/200/300',
+            fit: BoxFit.cover,
+          ),
+          const SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  '아이 런드',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+                const SizedBox(height: 14),
+                const Text(
+                  '이인혁',
+                  style: TextStyle(fontSize: 16),
+                ),
+                const Text(
+                  '2024.8.8 20:30',
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.w200),
+                ),
+                const SizedBox(height: 14),
+                Text(
+                  '플러터의 그리드 뷰를 배웠습니다' * 10,
+                  style: const TextStyle(fontSize: 16),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 
