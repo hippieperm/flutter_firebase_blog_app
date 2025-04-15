66bc2dc4d7d5a8fe482de238aaa814ac29a77db2
hippieperm <leess0133@gmail.com>
2025-04-16 02:31:45 +0900

    ✨ PostRepository 클래스에서 모든 포스트를 가져오는 기능 개선: Firestore에서 포스트 데이터를 리스트로 반환하고, 새로운 포스트 추가 기능 구현

--- lib/data/repository/post_repository.dart
+++ lib/data/repository/post_repository.dart
@@ -1,16 +1,39 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_firebase_blog_app/data/model/post.dart';
 
 class PostRepository {
-  Future<void> getAll() async {
+  Future<List<Post>> getAll() async {
     final firestore = FirebaseFirestore.instance;
     final collectionRef = firestore.collection('posts');
     final result = await collectionRef.get();
 
     final docs = result.docs;
 
-    for (var doc in docs) {
-      print(doc.id);
-      print(doc.data());
-    }
+    return docs.map((doc) {
+      final map = doc.data();
+      final newMap = {
+        'id': doc.id,
+        ...map,
+      };
+      return Post.fromJson(newMap);
+    }).toList();
+  }
+
+  Future<void> insert({
+    required String title,
+    required String content,
+    required String writer,
+    required String imageUrl,
+  }) async {
+    final firestore = FirebaseFirestore.instance;
+    final collectionRef = firestore.collection('posts');
+    final docRef = collectionRef.doc();
+    docRef.set({
+      'id': docRef.id,
+      'title': title,
+      'content': content,
+      'writer': writer,
+      'imageUrl': imageUrl,
+    });
   }
 }
