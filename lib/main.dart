016e13958684d2df63d9eb890c833a81647d6701
hippieperm <leess0133@gmail.com>
2025-04-16 17:12:48 +0900

    ✨ main.dart에서 home_page.dart의 경로를 수정하여 코드 구조를 간소화함

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_firebase_blog_app/ui/home/home_page.dart';
+import 'ui/home/home_page.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
