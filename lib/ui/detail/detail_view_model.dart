0c2112f9fb1bdc14e34adfc946c7bd47a923adda
hippieperm <leess0133@gmail.com>
2025-04-16 15:53:21 +0900

    ✨ DetailViewModel에 포스트의 실시간 업데이트를 수신하는 listenStream 메서드를 추가하고, 뷰모델이 생성될 때 해당 메서드를 호출하도록 수정

--- lib/ui/detail/detail_view_model.dart
+++ lib/ui/detail/detail_view_model.dart
@@ -5,11 +5,24 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 class DetailViewModel extends AutoDisposeFamilyNotifier<Post, Post> {
   @override
   Post build(Post post) {
+    listenStream(post.id);
     return post;
   }
 
   Future<bool> delete(Post post) async {
     return await PostRepository().delete(post.id);
+  }
+
+  void listenStream(String id) {
+    final stream = PostRepository().postStream(id);
+    final streamSubscription = stream.listen((post) {
+      if (post != null) {
+        state = post;
+      }
+    });
+    ref.onDispose(() {
+      streamSubscription.cancel();
+    });
   }
 }
 
